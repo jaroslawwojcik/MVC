559ab37b28228e13c7f8da87d653ae1c975dfd0f
Jarosław Wójcik <jaroslavwojcik@gmail.com>
2018-09-22 10:59:50 +0200

    First Commit

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            ViewBag.Message = DateTime.Now.ToString();
+
+            City city = new City
+            {
+                Name = "Warsaw",
+                StreetNumber = 5,
+                Street = "Aleje Jerozolimskie"
+            };
+
+
+            
+            return View(city);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Models/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+    }
+}
